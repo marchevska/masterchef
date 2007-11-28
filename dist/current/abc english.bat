8e6fe873c5ea065579ec48ac4e65ab1453b3224e
marchevska <olga.marchevska@gmail.com>
2007-11-28 14:53:52 +0000

    локализация логотипов - исключение файлов из репозитория

--- dist/current/abc english.bat
+++ dist/current/abc english.bat
@@ -9,6 +9,11 @@ copy "%cd%\abc english\img\cookbook\hawaiian\*.png" "%cd%\img\cookbook\hawaiian\
 copy "%cd%\abc english\img\cookbook\japanese\*.png" "%cd%\img\cookbook\japanese\*.png"
 copy "%cd%\abc english\img\cookbook\mexican\*.png" "%cd%\img\cookbook\mexican\*.png"
 copy "%cd%\abc english\img\cookbook\russian\*.png" "%cd%\img\cookbook\russian\*.png"
+copy "%cd%\abc english\img\episodes\american\*.png" "%cd%\img\episodes\american\*.png"
+copy "%cd%\abc english\img\episodes\hawaiian\*.png" "%cd%\img\episodes\hawaiian\*.png"
+copy "%cd%\abc english\img\episodes\japanese\*.png" "%cd%\img\episodes\japanese\*.png"
+copy "%cd%\abc english\img\episodes\mexican\*.png" "%cd%\img\episodes\mexican\*.png"
+copy "%cd%\abc english\img\episodes\russian\*.png" "%cd%\img\episodes\russian\*.png"
 copy "%cd%\abc english\img\fonts\*.png" "%cd%\img\fonts\*.png"
 copy "%cd%\abc english\img\gui\*.png" "%cd%\img\gui\*.png"
 
--- dist/current/abc russian.bat
+++ dist/current/abc russian.bat
@@ -9,6 +9,11 @@ copy "%cd%\abc russian\img\cookbook\hawaiian\*.png" "%cd%\img\cookbook\hawaiian\
 copy "%cd%\abc russian\img\cookbook\japanese\*.png" "%cd%\img\cookbook\japanese\*.png"
 copy "%cd%\abc russian\img\cookbook\mexican\*.png" "%cd%\img\cookbook\mexican\*.png"
 copy "%cd%\abc russian\img\cookbook\russian\*.png" "%cd%\img\cookbook\russian\*.png"
+copy "%cd%\abc russian\img\episodes\american\*.png" "%cd%\img\episodes\american\*.png"
+copy "%cd%\abc russian\img\episodes\hawaiian\*.png" "%cd%\img\episodes\hawaiian\*.png"
+copy "%cd%\abc russian\img\episodes\japanese\*.png" "%cd%\img\episodes\japanese\*.png"
+copy "%cd%\abc russian\img\episodes\mexican\*.png" "%cd%\img\episodes\mexican\*.png"
+copy "%cd%\abc russian\img\episodes\russian\*.png" "%cd%\img\episodes\russian\*.png"
 copy "%cd%\abc russian\img\fonts\*.png" "%cd%\img\fonts\*.png"
 copy "%cd%\abc russian\img\gui\*.png" "%cd%\img\gui\*.png"
 
